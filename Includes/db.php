97f6cb4df9221a9c9c2e12e3f404ee7c79f2a3c7
tumutech <ftumusiime73@gmail.com>
2023-04-16 15:49:31 +0300

    database added

--- Includes/db.php
+++ Includes/db.php
@@ -1,5 +1,4 @@
 <?php
-$conn = new mysqli("localhost", "betteras_internv", "betteras_logbook", "betteras_logbook");
+$conn = new mysqli("localhost", "betteras_intern", "betteras_logbook", "betteras_logbook");
 if ($conn) {
-    // echo("database connected");
-}else echo("error");
+}
